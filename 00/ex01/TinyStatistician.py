c26cec3589747844f32b1a58d98180a685d30ae3
jmcheon <developer.cheon@gmail.com>
2023-05-03 22:31:26 +0200

    feat: 00/ex01 done

--- 00/ex01/TinyStatistician.py
+++ 00/ex01/TinyStatistician.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class TinyStatistician:
 	"""
 		• mean(x), median(x), quartile(x), percentile(x, p), var(x), std(x)
@@ -36,12 +38,35 @@ class TinyStatistician:
 		q3 = self.median(numbers[(n + 1)//2:])
 		return [q1, q3]
 
-	def percentile(x, p):
+	def percentile(self, x, p):
 		"""
 		it computes the expected percentile of a given non-empty list or array x.
 		The method returns the percentile as a float, otherwise None if x is an empty list or array or a non expected type object.
 		The second parameter is the wished percentile. This method should not raise any Exception.
 		"""
+		if not isinstance(x, (np.ndarray, list)) or len(x) == 0:
+			return None
+		sorted_x = sorted(x)
+		#print("sorted x:", sorted_x, f"p:{p}%\n")
+		# Bessel's correction: len(x) - 1 instead of len(x)
+		index = (len(x) - 1) * p / 100.0
+		if index.is_integer():
+			return sorted_x[int(index)]
+		else:
+			k = int(index) # the index of lower value nearest to the desired percentile
+			d = index - k # decimal part of index
+			#print(f"index:{index}, k(int part):{k}, d(decimal part):{d}\n")
+			#print(f"sorted_x[{k}]: {sorted_x[k]}")
+			#print(f"sorted_x[{k + 1}]: {sorted_x[k + 1]}")
+
+			#print(f"sorted_x[{k}] * {1 - d}: {sorted_x[k] * (1 - d)}")
+			#print(f"sorted_x[{k + 1}] * {d}: {sorted_x[k + 1] * d}\n")
+
+		#	print(f"\nsorted_x[k] * (1 - d) + sorted_x[k + 1] * d")
+		#	print(f"sorted_x[{k}] * {1 - d} + sorted_x[{k + 1}] * {d}")
+		#	print(f"{sorted_x[k]} * {1 - d} + {sorted_x[k + 1]} * {d}")
+		#	print(f"{sorted_x[k] * (1 - d)} + {sorted_x[k + 1] * d}")
+			return sorted_x[k] * (1 - d) + sorted_x[k + 1] * d
 
 	def var(self, x):
 		if not isinstance(x, list) or len(x) == 0:
@@ -87,11 +112,15 @@ def ex4():
 	print(TinyStatistician().mean(a)) # Output: 82.4
 	print(TinyStatistician().median(a)) # Output: 42.0
 	print(TinyStatistician().quartile(a)) # Output: [10.0, 59.0]
-#	print(TinyStatistician().percentile(a, 10)) # Output: 4.6
-#	print(TinyStatistician().percentile(a, 15)) # Output: 6.4
-#	print(TinyStatistician().percentile(a, 20)) # Output: 8.2
 	print(TinyStatistician().var(a)) # Output: 15349.3
 	print(TinyStatistician().std(a)) # Output: 123.89229193133849
+
+def ex5():
+	a = [1, 42, 300, 10, 59]
+	print(TinyStatistician().percentile(a, 10)) # Output: 4.6
+	print(TinyStatistician().percentile(a, 15)) # Output: 6.4
+	print(TinyStatistician().percentile(a, 20)) # Output: 8.2
+	print(TinyStatistician().percentile(a, 80)) # Output: 8.2
 
 if __name__ == "__main__":
 	t = TinyStatistician()
