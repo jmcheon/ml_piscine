f6919dbc663c93de45dc987251934bd90a6bcc68
jmcheon <developer.cheon@gmail.com>
2023-05-06 13:21:27 +0200

    test: ex08

---
+++ 00/ex08/loss.py
@@ -0,0 +1,95 @@
+import numpy as np
+from prediction import predict_
+
+def loss_elem_(y, y_hat):
+	"""
+	Description:
+	Calculates all the elements (y_pred - y)^2 of the loss function.
+
+	Args:
+	y: has to be an numpy.array, a vector.
+	y_hat: has to be an numpy.array, a vector.
+
+	Returns:
+	J_elem: numpy.array, a vector of dimension (number of the training examples,1).
+	None if there is a dimension matching problem between X, Y or theta.
+	None if any argument is not of the expected type.
+
+	Raises:
+	This function should not raise any Exception.
+	"""
+	for v in [y, y_hat]:
+		if not isinstance(v, np.ndarray):
+			print(f"Invalid input: argument {v} of ndarray type required")	
+			return None
+
+	v = [y, y_hat]
+	for i in range(len(v)):
+		if v[i].ndim == 1:
+			v[i] = v[i].reshape(v[i].size, 1)
+		elif not (v[i].ndim == 2 and v[i].shape in [(v[i].size, 1), (1, v[i].size)]):
+			print(f"Invalid input: wrong shape of {v[i]}", v[i].shape)
+			return None
+
+	return (y_hat - y) ** 2
+	
+def loss_(y, y_hat):
+	"""
+	Description:
+	Calculates the value of loss function.
+
+	Args:
+	y: has to be an numpy.array, a vector.
+	y_hat: has to be an numpy.array, a vector.
+
+	Returns:
+	J_value : has to be a float.
+	None if there is a dimension matching problem between X, Y or theta.
+	None if any argument is not of the expected type.
+
+	Raises:
+	This function should not raise any Exception.
+	"""
+	for v in [y, y_hat]:
+		if not isinstance(v, np.ndarray):
+			print(f"Invalid input: argument {v} of ndarray type required")	
+			return None
+
+	v = [y, y_hat]
+	for i in range(len(v)):
+		print("bf:", v[i], v[i].shape)
+		if v[i].ndim == 1:
+			v[i] = v[i].reshape(v[i].size, 1)
+			print("test:", v[i], v[i].shape)
+		elif not (v[i].ndim == 2 and v[i].shape in [(v[i].size, 1), (1, v[i].size)]):
+			print(f"Invalid input: wrong shape of {v[i]}", v[i].shape)
+			return None
+
+	J_elem = loss_elem_(y, y_hat)
+	float_sum = float(np.sum(J_elem))
+	return float_sum / (2 * len(y))
+
+def ex1():
+	x1 = np.array([[0.], [1.], [2.], [3.], [4.]])
+	theta1 = np.array([[2.], [4.]])
+	y_hat1 = predict_(x1, theta1)
+	y1 = np.array([[2.], [7.], [12.], [17.], [22.]])
+	
+	# Example 1:
+	print(loss_elem_(y1, y_hat1)) # Output: array([[0.], [1], [4], [9], [16]])
+	# Example 2:
+	print(loss_(y1, y_hat1)) # Output: 3.0
+
+
+	x2 = np.array([0, 15, -9, 7, 12, 3, -21]).reshape(-1, 1)
+	theta2 = np.array([[0.], [1.]]).reshape(-1, 1)
+	y_hat2 = predict_(x2, theta2)
+	y2 = np.array([2, 14, -13, 5, 12, 4, -19]).reshape(-1, 1)
+	
+	# Example 3:
+	print(loss_(y2, y_hat2)) # Output: 2.142857142857143
+	# Example 4:
+	print(loss_(y2, y2)) # Output: 0.0
+
+if __name__ == "__main__":
+	ex1()
---
+++ 00/ex08/plot.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tools import add_intercept
+from loss import loss_
+
+def plot_with_loss(x, y, theta):
+	"""
+	Plot the data and prediction line from three non-empty numpy.ndarray.
+
+	Args:
+	x: has to be an numpy.ndarray, a vector of dimension m * 1.
+	y: has to be an numpy.ndarray, a vector of dimension m * 1.
+	theta: has to be an numpy.ndarray, a vector of dimension 2 * 1.
+
+	Returns:
+	Nothing.
+
+	Raises:
+	This function should not raise any Exception.
+	"""
+	for v in [x, y, theta]:
+		if not isinstance(v, np.ndarray):
+			print(f"Invalid input: argument {v} of ndarray type required")	
+			return
+
+	if x.ndim == 1:
+		x = x.reshape(x.size, 1)
+	elif not (x.ndim == 2 and x.shape[1] == 1):
+		print(f"Invalid input: wrong shape of {x}", x.shape)
+		return
+
+	if y.ndim == 1:
+		y = y.reshape(y.size, 1)
+	elif not (y.ndim == 2 and y.shape[1] == 1):
+		print(f"Invalid input: wrong shape of {y}", y.shape)
+		return
+	
+	if x.shape != y.shape:
+		print("Invalid input: shapes of x, y should be indentical")
+		return
+
+	if theta.ndim == 1 and theta.size == 2:
+		pass
+	elif not (theta.ndim == 2 and theta.shape == (2, 1)):
+		print("Invalid input: wrong shape of theta ", theta.shape)
+		return
+
+	X = add_intercept(x)
+	y_hat = X.dot(theta)
+	#cost = loss_(y, y_hat.reshape(y_hat.size, 1)) * 2
+	cost = loss_(y, y_hat)
+
+	fig, ax = plt.subplots()
+	ax.scatter(x, y, color='blue', label='data points')
+	ax.plot(np.arange(1, y_hat.size + 1), y_hat, color='orange', label='prediction line')
+	ax.legend()
+	plt.title(f"Cost : {cost:.6f}")
+	plt.show()
+
+
+def ex1():
+	x = np.arange(1,6)
+	y = np.array([11.52434424, 10.62589482, 13.14755699, 18.60682298, 14.14329568])
+
+	# Example 1:
+	theta1= np.array([18,-1])
+	plot_with_loss(x, y, theta1)
+	
+	# Example 2:
+	theta2 = np.array([14, 0])
+	plot_with_loss(x, y, theta2)
+	
+	# Example 3:
+	theta3 = np.array([12, 0.8])
+	plot_with_loss(x, y, theta3)
+
+if __name__ == "__main__":
+	ex1()
